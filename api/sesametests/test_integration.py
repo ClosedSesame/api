b8ff7e024423c4d2c3cc3dcc3b19a43b014cab85
Stephen <stephendeanharper@gmail.com>
2018-09-14 12:36:10 -0700

    all tests in test files

--- api/sesametests/test_integration.py
+++ api/sesametests/test_integration.py
@@ -53,3 +53,89 @@ def test_no_passwords_when_passing_invalid_account(testapp):
     }
     response = testapp.get('/api/v1/passwords', json.dumps(account), status='4**')
     assert response.status_code == 400
+
+
+def test_can_authenticate_registered_account(testapp):
+    """
+    """
+    account = {
+        'email': 'please@work.com',
+        'password': 'halo',
+    }
+    token = testapp.post('/api/v1/auth/login', json.dumps(account)).json['token']
+    testapp.authorization = ('Bearer', token)
+    response = testapp.get('/api/v1/passwords', json.dumps(account), status='2**')
+    assert response.status_code == 200
+
+
+def test_can_create_password_with_registered_account(testapp):
+    """
+    """
+    account = {
+        'email': 'please@work.com',
+        'password': 'halo',
+    }
+    token = testapp.post('/api/v1/auth/login', json.dumps(account)).json['token']
+    testapp.authorization = ('Bearer', token)
+    passwords = {'website': 'www.google.com', 'login': 'test@test.com', 'password': 'test'}
+    response = testapp.post('/api/v1/passwords', json.dumps(passwords), status='2**')
+    assert response.status_code == 201
+
+
+def test_fails_without_website_kwarg_for_registered_account(testapp):
+    """
+    """
+    account = {
+        'email': 'please@work.com',
+        'password': 'halo',
+    }
+    token = testapp.post('/api/v1/auth/login', json.dumps(account)).json['token']
+    testapp.authorization = ('Bearer', token)
+    passwords = {'notwebsite': 'www.google.com', 'login': 'test@test.com', 'password': 'test'}
+    response = testapp.post('/api/v1/passwords', json.dumps(passwords), status='4**')
+    assert response.status_code == 400
+
+
+def test_fails_without_login_kwarg_for_registered_account(testapp):
+    """
+    """
+    account = {
+        'email': 'please@work.com',
+        'password': 'halo',
+    }
+    token = testapp.post('/api/v1/auth/login', json.dumps(account)).json['token']
+    testapp.authorization = ('Bearer', token)
+    passwords = {'website': 'www.google.com', 'notlogin': 'test@test.com', 'password': 'test'}
+    response = testapp.post('/api/v1/passwords', json.dumps(passwords), status='4**')
+    assert response.status_code == 400
+
+
+def test_fails_without_password_kwarg_for_registered_account(testapp):
+    """
+    """
+    account = {
+        'email': 'please@work.com',
+        'password': 'halo',
+    }
+    token = testapp.post('/api/v1/auth/login', json.dumps(account)).json['token']
+    testapp.authorization = ('Bearer', token)
+    passwords = {'website': 'www.google.com', 'login': 'test@test.com', 'notpassword': 'nottest'}
+    response = testapp.post('/api/v1/passwords', json.dumps(passwords), status='4**')
+    assert response.status_code == 400
+
+
+def test_passes_when_listing_all_passwords_for_registered_account(testapp):
+    """
+    """
+    account = {
+        'email': 'please@work.com',
+        'password': 'halo',
+    }
+    token = testapp.post('/api/v1/auth/login', json.dumps(account)).json['token']
+    testapp.authorization = ('Bearer', token)
+    passwords = {'website': 'www.notgoogle.com', 'login': 'test@test.com', 'password': 'test'}
+    testapp.authorization = ('Bearer', token)
+    testresponse = testapp.post('/api/v1/passwords', json.dumps(passwords), status='2**')
+    print('this is test response', testresponse)
+    request = testapp.get('/api/v1/passwords')
+    assert request.status_code == 200
